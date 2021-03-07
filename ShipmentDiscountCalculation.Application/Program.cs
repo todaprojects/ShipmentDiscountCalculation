f03e50eebe78cc88f996664e0811dcaec8bed2bb
Tomas Daugela <todaprojects@gmail.com>
2021-03-07 21:03:33 +0200

    Delegate Shipment Discount Calculation to separate class

--- ShipmentDiscountCalculation.Application/Program.cs
+++ ShipmentDiscountCalculation.Application/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Microsoft.Extensions.Configuration;
 using ShipmentDiscountCalculation.Application.DataServices;
-using ShipmentDiscountCalculation.Application.Factories;
-using ShipmentDiscountCalculation.Application.Helpers;
 using ShipmentDiscountCalculation.Application.Interfaces;
-using ShipmentDiscountCalculation.Domain.Models;
+using ShipmentDiscountCalculation.Domain.Interfaces;
 
 namespace ShipmentDiscountCalculation.Application
 {
@@ -16,47 +9,23 @@ namespace ShipmentDiscountCalculation.Application
     {
         static void Main(string[] args)
         {
-            IDataInputService inputService = new FileDataInputService();
             IDataOutputService outputService = new ConsoleDataOutputService();
-            
-            // Get the list of available Shipping Providers in selected country
-            var carriers = CarrierFactory.GetShippingProviders();
-            
-            // Initialize all Transactions into Shipping objects - for processing shipment discount calculations
-            var shipments = new List<Shipment>();
-            
-            var runApp = true;
-            while (runApp)
+
+            IShipmentDiscountCalculation shipmentDiscountCalculation = new ShipmentDiscountCalculation();
+
+            while (true)
             {
                 try
                 {
-                    var inputLine = inputService.GetData();
-                    if (inputLine == null)
+                    var shipmentDiscountData = shipmentDiscountCalculation.GetShipmentDiscountData();
+                    if (shipmentDiscountData != null)
                     {
-                        runApp = false;
+                        outputService.Print(shipmentDiscountData);
+                    }
+                    else
+                    {
                         break;
                     }
-            
-                    var inputs = inputLine.Split(" ");
-            
-                    var date = DateParseHelper.Parse(inputs, 0);
-                    var packageSize = PackageSizeParseHelper.Parse(inputs, 1);
-                    var carrier = CarrierParseHelper.Parse(carriers, inputs, 2);
-                    var priceService = ShippingPriceFactory.GetPriceService(packageSize);
-            
-                    var shipment = new Shipment(date, packageSize, carrier, priceService);
-            
-                    shipment.ShippingPrice =
-                        shipment.PriceService.GetShippingPrice(carriers, shipments, shipment);
-            
-                    shipment.Discount =
-                        shipment.Carrier.Packages
-                            .Single(p => p.Size == packageSize)
-                            .Price - shipment.ShippingPrice;
-            
-                    shipments.Add(shipment);
-            
-                    outputService.Print(shipment);
                 }
                 catch (ArgumentException e)
                 {
@@ -65,7 +34,7 @@ namespace ShipmentDiscountCalculation.Application
                 catch (Exception e)
                 {
                     outputService.ErrorMsg($"Error: {e.Message}");
-                    runApp = false;
+                    break;
                 }
             }
         }
