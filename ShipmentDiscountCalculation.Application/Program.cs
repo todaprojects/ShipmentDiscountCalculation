abe1e1004b2639d3b92d530abc47d68d3d67b89b
Tomas Daugela <todaprojects@gmail.com>
2021-03-07 20:06:01 +0200

    Refactor 'input file' declaration to appsettings.json

--- ShipmentDiscountCalculation.Application/Program.cs
+++ ShipmentDiscountCalculation.Application/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
 using ShipmentDiscountCalculation.Application.DataServices;
 using ShipmentDiscountCalculation.Application.Factories;
 using ShipmentDiscountCalculation.Application.Helpers;
@@ -12,20 +14,17 @@ namespace ShipmentDiscountCalculation.Application
 {
     class Program
     {
-        private static readonly char Separator = Path.DirectorySeparatorChar;
-        private static readonly string InputFilePath = $"..{Separator}..{Separator}..{Separator}..{Separator}Files{Separator}input.txt";
-
         static void Main(string[] args)
         {
-            IInputService inputService = new FileInputService(FilePathHelper.GetInputFilePath(InputFilePath, Separator));
-            IOutputService outputService = new ConsoleOutputService();
-
+            IDataInputService inputService = new FileDataInputService();
+            IDataOutputService outputService = new ConsoleDataOutputService();
+            
             // Get the list of available Shipping Providers in selected country
             var carriers = CarrierFactory.GetShippingProviders();
-
+            
             // Initialize all Transactions into Shipping objects - for processing shipment discount calculations
             var shipments = new List<Shipment>();
-
+            
             var runApp = true;
             while (runApp)
             {
@@ -37,26 +36,26 @@ namespace ShipmentDiscountCalculation.Application
                         runApp = false;
                         break;
                     }
-
+            
                     var inputs = inputLine.Split(" ");
-
+            
                     var date = DateParseHelper.Parse(inputs, 0);
                     var packageSize = PackageSizeParseHelper.Parse(inputs, 1);
                     var carrier = CarrierParseHelper.Parse(carriers, inputs, 2);
                     var priceService = ShippingPriceFactory.GetPriceService(packageSize);
-
+            
                     var shipment = new Shipment(date, packageSize, carrier, priceService);
-
+            
                     shipment.ShippingPrice =
                         shipment.PriceService.GetShippingPrice(carriers, shipments, shipment);
-
+            
                     shipment.Discount =
                         shipment.Carrier.Packages
                             .Single(p => p.Size == packageSize)
                             .Price - shipment.ShippingPrice;
-
+            
                     shipments.Add(shipment);
-
+            
                     outputService.Print(shipment);
                 }
                 catch (ArgumentException e)
